edbc6a405495f84f125459be1546196b15003d83
Marc <dlcmarc32@gmail.com>
2025-02-17 10:07:48 +0100

    fromulario basico fiel al diseño. solo 1 componente
    formulario basico ya podría ser funcional. es fiel al diseño original. solo tiene un componente. podría ser mucho mejor pero bueno. Plugin: Figma to code. Genera solo un html con el css incrustado. Bastante rústico

--- projects/chorizus-mi-biblioteca/src/public-api.ts
+++ projects/chorizus-mi-biblioteca/src/public-api.ts
@@ -2,7 +2,8 @@
  * Public API Surface of chorizus-mi-biblioteca
  */
 
-export * from './lib/chorizus-mi-biblioteca.service';
+// export * from './lib/chorizus-mi-biblioteca.service';
 export * from './lib/chorizus-mi-biblioteca.component';
 export * from './lib/chorizus-mi-biblioteca.module';
 export * from './lib/form-component/form-component.component';
+
